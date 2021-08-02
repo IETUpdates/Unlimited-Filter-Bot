908c2c94b516473b7bd6133628d627f7038c6ba2
IET Updates <83505196+IETUpdates@users.noreply.github.com>
2021-08-02 17:41:41 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -243,12 +243,12 @@ async def help(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
+                    InlineKeyboardButton("🎥 For Movies", url="https://t.me/mm_moviess"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("📽️ For Series", url="https://t.me/mm_seriess"),
+                    InlineKeyboardButton("👩‍💻 Devloper", url="https://t.me/iet_owner")
                 ]
             ]
         ),
@@ -265,7 +265,7 @@ async def about(client, message):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "SOURCE CODE", url="https://t.me/IET_Owner/724")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
